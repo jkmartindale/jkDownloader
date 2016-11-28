7853be30568f165ca64f64c312ceb59369100bda
James Martindale <jkmartindale@users.noreply.github.com>
2016-11-28 06:17:51 -0700

    Create loader.js

---
+++ loader.js
@@ -0,0 +1,1 @@
+javascript:(function(){var jkDownloader=document.createElement("script");jkDownloader.setAttribute("src","https://jkmartindale.com/jkDownloader.js");document.body.appendChild(jkDownloader)}());
